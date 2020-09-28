07bdc5d944f410e3c8f87a881f94765b44d07f2b
Alexey Dikarev <mailsub@yandex.ru>
2020-09-28 11:02:32 +0300

    deploy

---
+++ precache-manifest.a0bebe6b83bd4c7d945514ab9a0a65fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a3df363087c8affa9a",
+    "url": "/vue-data-table/css/app.581506e4.css"
+  },
+  {
+    "revision": "377222ae613206c47ff39ee4fcb240c9",
+    "url": "/vue-data-table/img/close.377222ae.svg"
+  },
+  {
+    "revision": "5927eaaffd049b2992fa0f52de7121ea",
+    "url": "/vue-data-table/index.html"
+  },
+  {
+    "revision": "b4a3df363087c8affa9a",
+    "url": "/vue-data-table/js/app.93481670.js"
+  },
+  {
+    "revision": "484d44c3ee40ebf61d31",
+    "url": "/vue-data-table/js/chunk-vendors.ac05468a.js"
+  },
+  {
+    "revision": "176311716ff899e06d6175d213dcddb6",
+    "url": "/vue-data-table/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-data-table/robots.txt"
+  }
+]);
